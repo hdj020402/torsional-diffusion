a094ed736143199ffbefc5ad79cd8636bf7e8cca
hdj <hudejun2002@gmail.com>
2025-10-03 23:41:39 +0800

    fix(main): add PYTHONHASHSEED environment variable
    Set PYTHONHASHSEED to 0 to ensure reproducible hash values across
    different Python processes. This helps maintain consistent behavior
    when running the application multiple times.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os, time, yaml, json, pickle
 os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+os.environ["PYTHONHASHSEED"] = "0"
 import torch, optuna
 from multiprocessing import Pool
 from functools import partial
